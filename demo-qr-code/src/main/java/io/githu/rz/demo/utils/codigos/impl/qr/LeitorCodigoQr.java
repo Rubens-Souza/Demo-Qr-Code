d87a7561efdafc73371b9046dd8dccd6222c2802
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-09-24 12:17:27 -0300

    fix(utils): Adjust regex to remove base64 header

--- demo-qr-code/src/main/java/io/githu/rz/demo/utils/codigos/impl/qr/LeitorCodigoQr.java
+++ demo-qr-code/src/main/java/io/githu/rz/demo/utils/codigos/impl/qr/LeitorCodigoQr.java
@@ -2,9 +2,7 @@ package io.githu.rz.demo.utils.codigos.impl.qr;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.util.Base64;
-import java.util.regex.Pattern;
 
 import javax.imageio.ImageIO;
 
@@ -26,44 +24,45 @@ public class LeitorCodigoQr<T> implements ILeitorCodigo<T> {
 
 	private final String MSG_ERRO_LEITURA_CODIGO_QR = "Erro ao ler dados no base64 do código QR: %s";
 	private final String MSG_ERRO_QR_NAO_IDENTIFICADO = "O código de barras no Qr code não pode ser corretamente identificado: %s";
-	
+
 	@Override
 	public T lerCodigoBase64(String codigo, Class<T> classeGerada) {
 		String imagemFormatada = removerCabecalhoBase64(codigo);
 		BinaryBitmap bitmapCodigoQr = converterBase64(imagemFormatada);
-		
+
 		Result dados;
 		try {
 			dados = new MultiFormatReader().decode(bitmapCodigoQr);
 		} catch (NotFoundException ex) {
 			throw new LeituraCodigoException(String.format(MSG_ERRO_QR_NAO_IDENTIFICADO, ex.getMessage()));
 		}
-		
+
 		T dadosConvertidos = converterTextoCodigoQr(dados.getText(), classeGerada);
 		return dadosConvertidos;
 	}
-	
+
 	private String removerCabecalhoBase64(String base64) {
-		final String regexPadraoCabecalhoBase64 = "^data:image\\/[a-zA-Z]+;base64,";
-		return base64.replace(regexPadraoCabecalhoBase64, StringUtils.EMPTY);
+		final String regexPadraoCabecalhoBase64 = "^data:image/[^;]*;base64,?";
+		return base64.replaceFirst(regexPadraoCabecalhoBase64, StringUtils.EMPTY);
 	}
-	
+
 	private BinaryBitmap converterBase64(String base64) {
 		byte[] imagemBytes = Base64.getDecoder().decode(base64);
 		ByteArrayInputStream inputStreamImagem = new ByteArrayInputStream(imagemBytes);
-		
-		BufferedImage imagemCodigoQr;
+
+		BinaryBitmap bitmap;
 		try {
-			imagemCodigoQr = ImageIO.read(inputStreamImagem);
-		} catch (IOException ex) {
-			throw new LeituraCodigoException(String.format(MSG_ERRO_LEITURA_CODIGO_QR, ex.getMessage()));
+			BufferedImage imagemCodigoQr = ImageIO.read(inputStreamImagem);
+			LuminanceSource sourceImagem = new BufferedImageLuminanceSource(imagemCodigoQr);
+			bitmap = new BinaryBitmap(new HybridBinarizer(sourceImagem));
+		} catch (Exception ex) {
+			throw new LeituraCodigoException(
+					String.format(MSG_ERRO_LEITURA_CODIGO_QR, StringUtils.defaultString(ex.getMessage())));
 		}
-		
-		LuminanceSource sourceImagem = new BufferedImageLuminanceSource(imagemCodigoQr);
-	    BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(sourceImagem));
-	    return bitmap;
+
+		return bitmap;
 	}
-	
+
 	private T converterTextoCodigoQr(String dadosCodigoQr, Class<T> classeGerada) {
 		Gson gson = new Gson();
 		return gson.fromJson(dadosCodigoQr, classeGerada);
