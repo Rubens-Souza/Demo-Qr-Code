5a3e3ed87c3724673c34db40b51c684882999812
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-09-24 11:30:31 -0300

    feat(services): Implements Pagamento service

--- demo-qr-code/src/main/java/io/github/rz/demo/services/PagamentoCodigoService.java
+++ demo-qr-code/src/main/java/io/github/rz/demo/services/PagamentoCodigoService.java
@@ -1,9 +1,24 @@
 package io.github.rz.demo.services;
 
+import org.springframework.stereotype.Service;
+
+import io.githu.rz.demo.utils.codigos.IManipuladorCodigos;
+import io.githu.rz.demo.utils.codigos.impl.qr.ManipuladorCodigosQr;
+import io.github.rz.demo.dto.CodigoQrDTO;
+import io.github.rz.demo.dto.PagamentoDTO;
+
+@Service
 public class PagamentoCodigoService {
 
-	public String gerarCodigoPagamento() {
-		return "";
+	private IManipuladorCodigos<PagamentoDTO> manipuladorCodigos = new ManipuladorCodigosQr<PagamentoDTO>();
+
+	public CodigoQrDTO gerarCodigoPagamento(PagamentoDTO pagamento) {
+		String codigoGerado = manipuladorCodigos.gerarCodigo(pagamento);
+		return CodigoQrDTO.builder().formatoImagem("png").altura(100).largura(100).imagemBase64(codigoGerado).build();
 	}
 	
+	public PagamentoDTO lerCodigoPagamento(String codigoPagamentoBase64) {
+		PagamentoDTO dadosPagamento = manipuladorCodigos.lerCodigo(codigoPagamentoBase64, PagamentoDTO.class);
+		return dadosPagamento;
+	}
 }
