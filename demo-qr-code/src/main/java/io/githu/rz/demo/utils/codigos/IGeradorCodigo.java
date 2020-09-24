d5bc0550570ae0a17af090594249ab01c476b5b0
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-09-24 12:34:33 -0300

    feat(utils): Add parameters to change the size and the image format

--- demo-qr-code/src/main/java/io/githu/rz/demo/utils/codigos/IGeradorCodigo.java
+++ demo-qr-code/src/main/java/io/githu/rz/demo/utils/codigos/IGeradorCodigo.java
@@ -2,6 +2,6 @@ package io.githu.rz.demo.utils.codigos;
 
 public interface IGeradorCodigo<T> {
 
-	public String criarCodigoBase64(T dados);
+	public String criarCodigoBase64(T dados, Integer altura, Integer largura, String formato);
 	
 }
--- demo-qr-code/src/main/java/io/githu/rz/demo/utils/codigos/IManipuladorCodigos.java
+++ demo-qr-code/src/main/java/io/githu/rz/demo/utils/codigos/IManipuladorCodigos.java
@@ -1,8 +1,13 @@
 package io.githu.rz.demo.utils.codigos;
 
+import io.githu.rz.demo.utils.codigos.enums.FormatoImagens;
+
 public interface IManipuladorCodigos<T> {
 
 	public String gerarCodigo(T dados);
+	public String gerarCodigo(T dados, Integer altura, Integer largura);
+	public String gerarCodigo(T dados, Integer altura, Integer largura, FormatoImagens formato);
+	
 	public T lerCodigo(String codigo, Class<T> classeGerada);
 	
 }
