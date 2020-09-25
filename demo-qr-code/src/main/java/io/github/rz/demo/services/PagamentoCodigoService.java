96a7ef79720c7eed513e3eb22296d98bfed64fd7
Rubens Souza <rubens.souza@dtidigital.com.br>
2020-09-25 15:30:02 -0300

    refactor(service): Fix service qr code generation

--- demo-qr-code/src/main/java/io/github/rz/demo/services/PagamentoCodigoService.java
+++ demo-qr-code/src/main/java/io/github/rz/demo/services/PagamentoCodigoService.java
@@ -4,8 +4,7 @@ import org.springframework.stereotype.Service;
 
 import io.github.rz.demo.dto.CodigoQrDTO;
 import io.github.rz.demo.dto.PagamentoDTO;
-import io.github.rz.demo.utils.codigos.IManipuladorCodigos;
-import io.github.rz.demo.utils.codigos.impl.qr.ManipuladorCodigosQr;
+import io.github.rz.demo.utils.codigos.qr.CodigoQr;
 import io.github.rz.demo.utils.enums.FormatoImagens;
 
 @Service
@@ -16,15 +15,13 @@ public class PagamentoCodigoService {
 	
 	private final FormatoImagens FORMATO_QR_CODE = FormatoImagens.PNG;
 
-	private IManipuladorCodigos<PagamentoDTO> manipuladorCodigos = new ManipuladorCodigosQr<PagamentoDTO>();
-
 	public CodigoQrDTO gerarCodigoPagamento(PagamentoDTO pagamento) {
-		String codigoGerado = manipuladorCodigos.gerarCodigo(pagamento, ALTURA_QR_CODE, LARGURA_QR_CODE, FORMATO_QR_CODE);
+		String codigoGerado = CodigoQr.gerarCodigo(pagamento).comDimensoes(LARGURA_QR_CODE, ALTURA_QR_CODE).noFormato(FORMATO_QR_CODE).emBase64();
 		return CodigoQrDTO.builder().formatoImagem(FORMATO_QR_CODE.getFormato()).altura(ALTURA_QR_CODE).largura(LARGURA_QR_CODE).imagemBase64(codigoGerado).build();
 	}
 	
 	public PagamentoDTO lerCodigoPagamento(String codigoPagamentoBase64) {
-		PagamentoDTO dadosPagamento = manipuladorCodigos.lerCodigo(codigoPagamentoBase64, PagamentoDTO.class);
+		PagamentoDTO dadosPagamento = CodigoQr.lerCodigo(codigoPagamentoBase64, PagamentoDTO.class);
 		return dadosPagamento;
 	}
 }
